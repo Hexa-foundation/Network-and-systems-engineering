6b9642164b232637c3090ef2dd382bc773fe44bc
Andrew Mbugua <andrewmbugua388@gmail.com>
2021-01-05 11:56:49 +0300

    Update number_descending_order_using.rs

--- Rust/number_descending_order_using.rs
+++ Rust/number_descending_order_using.rs
@@ -1,3 +1,5 @@
+//Two part code
+//Part one: Numbers in descending order with a funtion
 //the sort funtion arranges in ascending order
 //this code arranges it in descending order
 
@@ -15,4 +17,28 @@ fn desc(a:&i32, b:&i32) -> Ordering{
 }
 arr.sort_by(desc);
 print!("{:?}",arr);
+}
+
+
+//Part two:number descending order but with closures
+
+use std::cmp::Ordering;
+
+fn main()
+{
+	let mut arr = [4,8,1,10,0,45,12,7];
+	
+	
+	//used a closure instead of a funtion
+	let desc = |a:&i32, b:&i32| -> Ordering {
+		if a < b {Ordering::Greater}
+		
+		else if a > b {Ordering::Less}
+		
+		else { Ordering::Equal }
+		
+};	
+	arr.sort_by(desc);
+	print!("{:?}",arr);
+	
 }
