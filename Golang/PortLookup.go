41b9f28d96b26aac03daca20359e25ada0bd1002
Andrew <andrewmbugua388@gmail.com>
2021-02-09 14:10:26 +0300

    Port lookup

---
+++ Golang/PortLookup.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+
+"fmt"
+"os"
+"net"
+
+)
+
+func main(){
+if len(os.Args) != 3 {
+	fmt.Fprintf(os.Stderr, "Usage: %s network-type service",
+	            os.Args[0])
+	            os.Exit(1)	
+	}
+	networkType := os.Args[1]
+	service := os.Args[2]
+	
+	port,err := net.LookupPort(networkType,service)
+	if err != nil{
+		fmt.Println("Error:",err.Error())
+		os.Exit(2)
+	}
+	fmt.Println("Service port",port)
+	os.Exit(0)
+
+}
+
+/* Result
+ * 
+ * $ ./PortLookup
+ * $ Service tcp telnet
+ * $ Service port 23 
+*/
+
